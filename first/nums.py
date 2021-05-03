a07d645d8aadba1c2524c6511554128d1315e720
dmitrprokofiev <svistoplyas92@gmail.com>
2021-05-03 18:42:43 +0300

    Добавлена модуль strings.py и добавлена функция mul()

--- first/nums.py
+++ first/nums.py
@@ -1,9 +1,11 @@
 def plus(a,b):
     return a*b
-text = 'new text'
+def mul(a,b):
+    return a+b
 
 def minus(a,b):
     return a-b
 if __name__ == '__main__':
     print(plus(2,2))
     print(minus(6,2))
+    print(mul(8,3))
